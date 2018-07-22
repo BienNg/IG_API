b148af4897508871e2cd151972f6c382b4f5aa84
Bien Nguyen <bien.nguyen@hotmail.de>
2018-07-22 22:30:01 +0200

    Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    private final String ACCESS_TOKEN = "269147980.3662590.9d3db94e0d934d2088a7eb13ffa15e49";
+    private final String BASE_URL = "https://api.instagram.com/";
+
+    public static void main(String[] args){
+
+    }
+}
